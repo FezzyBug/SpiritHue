c49c7f776965676374a408b529384c91cd82fb27
JozefVargas <futureisnear2050@gmail.com>
2023-03-26 19:55:30 -0400

    Fixed the interaction with HuggingFace API to now get the greatest similarity attribute for the text.

--- SpiritHueProject/SpiritHue/EmotionsCall.swift
+++ SpiritHueProject/SpiritHue/EmotionsCall.swift
@@ -17,7 +17,7 @@ func fetchEmotion(text: String, completion: @escaping (String?, Error?) -> Void)
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("Bearer YOUR_READ_TOKEN_HERE", forHTTPHeaderField: "Authorization")
+    request.addValue("Bearer YOUR_API_KEY", forHTTPHeaderField: "Authorization")
     
     // Define the JSON payload
     let json: [String: Any] = ["inputs": text]
@@ -30,24 +30,48 @@ func fetchEmotion(text: String, completion: @escaping (String?, Error?) -> Void)
             completion(nil, error)
             return
         }
-        // Print the JSON response in a readable format
-                do {
-                    let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                    print("JSON response:", jsonResult)
-                } catch let error {
-                    print("Error parsing JSON:", error)
+        // Print the JSON response in a readable format //Also Parses data
+//                do {
+//                    let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+//
+//
+//                    print("JSON response:", jsonResult)
+//                } catch let error {
+//                    print("Error parsing JSON:", error)
+//                }
+        do {
+            if let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [[Any]] {
+                // This get the first element of the mutable array that the JSON data is pass back in
+                // additionally, the first?.first points that the first (and highest value) key pair associated with the wmotions scale.
+                if let firstElement = jsonResult.first?.first {
+                    print("First element:", firstElement)
+                } else {
+                    print("Array is empty")
                 }
-                
+            } else {
+                print("Could not cast JSON response to array of arrays")
+            }
+        } catch let error {
+            print("Error parsing JSON:", error)
+        }
+
+//
+        
+        
+        
+        
+        
                 // Parse the response
-                do {
-                    if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                       let emotionArray = jsonResult["outputs"] as? [String] {
-                        let emotion = emotionArray.joined(separator: ", ")
-                        completion(emotion, nil)
-                    }
-                } catch let error {
-                    completion(nil, error)
-                }
+//                do {
+//                    if let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+//                       let emotionArray = jsonResult["outputs"] as? [String] {
+//                        let emotion = emotionArray.joined(separator: ", ")
+//                        print(jsonResult["outputs"])
+//                        completion(emotion, nil)
+//                    }
+//                } catch let error {
+//                    completion(nil, error)
+//                }
             }
             
             task.resume()
